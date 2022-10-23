891e5b2f63135c6fb7625af56c15993741d87c2f
mohammedramadan99 <lifeisagame0101@gmail.com>
2022-10-23 14:25:56 +0200

    s

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
 import Banner from "../components/banner/Banner"
 import Logo from "../components/logo/Logo"
-import Portfolio from "../components/portfolio/portfolio"
+import Portfolio from "../components/portfolio/Portfolio"
 import Features from "../components/sections/Features"
 import TextBlockOne from "../components/text-blocks/TextBlockOne"
 import TextBlockThree from "../components/text-blocks/TextBlockThree"
